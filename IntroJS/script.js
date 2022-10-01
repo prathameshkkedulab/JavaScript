65cb7f19a396188a4cbf60f88fcbd9f7331485ad
prathameshkkedulab <prathameshkkedulab>
2022-10-01 17:46:27 +0530

    new

--- IntroJS/script.js
+++ IntroJS/script.js
@@ -1,33 +1,12 @@
 // var date = new Date();
 // document.body.innerHTML = "<h1>Today is :" + date + "</h1>";
-//const btn = document.querySelector("button");
-// const para = document.querySelector("p");
-// var result = a+b;
-//btn.addEventListener('click', updatebtn);
-function add()
-{
-  // "use strict";
-let  a = parseInt(document.getElementById("a").value);
-let  b = parseInt(document.getElementById("b").value);
-let c = a + b;
- parseInt(document.getElementById("result")).value = c;
-}
-//
-// let btn = document.querySelector("button");
-// btn.onclick = () => {
-//   let b = document.querySelector("button");
-//   b.innerHTML = "Helo";
-// }
 
-// let c = document.getElementById("btn");
-// console.log(c);
-// if(c)
-// {
-//   c.addEventListener('click', () => {
-//     const a = document.getElementByClassName("a").value;
-//     const b = document.getElementByClassName("b").value;
-//     add = a + b;
-//     document.write(add);
-//     alert("jhg")
-//   })
-// }
+
+function add(){
+  let x = Number(document.getElementById('a').value);
+  let y = Number(document.getElementById('b').value);
+
+  document.getElementById('sum').value = x+y;
+}
+
+document.getElementById('run').addEventListener('click',add);
